d97fe754610cfd5b0b31eb0d7074a2359accf991
devbysalas <rgkalin@gmail.com>
2018-12-20 14:52:54 -0800

    Added basic functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log('Beautiful faces!');
+
+const todoInputEl = document.querySelector('.todo__input');
+const todoListEl = document.querySelector('.todo__list');
+const todoItemEls = document.querySelectorAll('.todo__item');
+
+function addItem() {
+  todoInputEl.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {
+      let newListItem = createListItem(todoInputEl.value);
+      todoListEl.insertBefore(newListItem, todoListEl.childNodes[0]);
+      todoInputEl.value = '';
+    }
+  });
+}
+
+function createListItem(text) {
+  const newListElement = document.createElement('li');
+  newListElement.textContent = text;
+  newListElement.setAttribute('class', 'todo__item');
+  return newListElement;
+}
+
+function toggleDone() {
+  todoListEl.addEventListener('click', function(event) {
+    if (event.target.classList.contains('todo__item')) {
+      event.target.classList.toggle('done');
+    }
+  });
+}
+
+toggleDone();
+
+addItem();
